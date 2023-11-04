bdec328d55647599ce5c8f26a31f685dd72cbc10
Marcin Adamiak <marcin_89@outlook.com>
2023-11-04 15:49:17 +0100

    Add: persisting selected locale in local storage

--- src/components/App.js
+++ src/components/App.js
@@ -7,13 +7,19 @@ import { messages } from '../i18n/messages';
 import { useState } from 'react';
 
 const App = () => {
-  const locale = LOCALES.ENGLISH;
-
-  const [currentLocale, setCurrentLocale] = useState(locale);
+  const [currentLocale, setCurrentLocale] = useState(getInitialLocal());
 
   const handleChange = (e) => {
     setCurrentLocale(e.target.value);
+    // storing locale in the localstorage
+    localStorage.setItem('locale', e.target.value);
   };
+
+  function getInitialLocal() {
+    // getting stored items
+    const savedLocale = localStorage.getItem('locale');
+    return savedLocale || LOCALES.ENGLISH;
+  }
 
   return (
     <IntlProvider messages={messages[currentLocale]} locale={currentLocale} defaultLocale={LOCALES.ENGLISH}>
